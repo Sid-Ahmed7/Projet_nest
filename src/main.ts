041bbe13b73578ce1ac365032101951a756fe13d
Sid-Ahmed7 <sido91080@hotmail.fr>
2026-04-02 09:00:52 +0200

    add game

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SeederService } from './seeder/seeder.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.get(SeederService).seedAdmin();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
