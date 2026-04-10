61eb63c246f3e5fdcec67046405fd7cb7ae609a4
Sofiane CHADILI <sofiane.chadili@hotmail.com>
2026-04-10 21:31:41 +0200

    add matches filter

--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -19,7 +19,13 @@ export class MatchService {
     private readonly tournamentRepository: Repository<Tournament>,
   ) {}
 
-  async findAll(): Promise<Match[]> {
+  async findAll(status?: MatchStatus): Promise<Match[]> {
+    if (status) {
+      return this.matchRepository.find({
+        where: { status },
+        relations: ['tournament', 'firstPlayer', 'secondPlayer', 'winner'],
+      });
+    }
     return this.matchRepository.find({
       relations: ['tournament', 'firstPlayer', 'secondPlayer', 'winner'],
     });
